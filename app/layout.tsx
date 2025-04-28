2ca30c8f9b00fc202488a91cccc9b55146255472
egegl <gurselege09@gmail.com>
2025-04-28 03:38:48 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { EB_Garamond } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const ebGaramond = EB_Garamond({
     subsets: ["latin"],
